ab4d0f5e22c288748435aab8ba89fced7e33038f
dlatldhs <80656700+dlatldhs@users.noreply.github.com>
2021-11-01 10:51:40 +0900

    Add files via upload

---
+++ database/Database.py
@@ -0,0 +1,17 @@
+import pymysql
+import time
+
+class Database:
+    def __init__(self):
+        self.db = pymysql.connect(host='localhost', user = 'ubuntu', password = 'dlatldhs',db = 'test')
+        self.cur = self.db.cursor()
+        print("nice!")
+
+    def insertJson(self,finddust):
+        tm = time.localtime()
+        timedate ="{}-{}-{}_{}:{}".format(tm.tm_year,tm.tm_mon,tm.tm_mday,tm.tm_hour,tm.tm_min)
+        sql="INSERT INTO dust(data) values(%s);"
+        val=('{{"time":"{}", "find-dust":"{}"}}'.format(timedate,finddust))
+        self.cur.execute(sql,val)
+        self.db.commit()
+        return "nice!"
