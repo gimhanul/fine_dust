f388ab2ded3f98bc5005a714c740f26754186975
dlatldhs <80656700+dlatldhs@users.noreply.github.com>
2021-11-08 10:24:32 +0900

    Add files via upload

--- database/Database.py
+++ database/Database.py
@@ -7,6 +7,14 @@ class Database:
         self.cur = self.db.cursor()
         print("nice!")
 
+    #데이터 출력
+    def selectAllJson(self):
+        sql = "select * from dust;"
+        self.cur.execute(sql)
+        result = self.cur.fetchall()
+        return result
+
+    #시간 마다 미세먼지 측정 값
     def insertJson(self,finddust):
         tm = time.localtime()
         timedate ="{}-{}-{}_{}:{}".format(tm.tm_year,tm.tm_mon,tm.tm_mday,tm.tm_hour,tm.tm_min)
