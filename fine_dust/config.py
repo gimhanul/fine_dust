f137aae7eb937aa9e4ee9b215b8c02d8693fd37d
gimhanul <gimhanuly@gmail.com>
2021-11-15 10:54:33 +0900

    Connect DB

---
+++ fine_dust/config.py
@@ -0,0 +1,7 @@
+db = {
+    'user' : 'ubuntu',
+    'password' : 'dlatldhs',
+    'host' : 'localhost',
+    'port' : '3306',
+    'database' : 'fine_dust_db',
+}
---
+++ main/models.py
@@ -0,0 +1,19 @@
+from enum import unique
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'user_table'
+   
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    email = db.Column(db.String(20), unique = True, nullable = False)
+    password = db.Column(db.String(20))
+    name = db.Column(db.String(10))
+
+    def __init__(self, email, password, name):
+        self.email = email
+        self.name = name
+        self.password = password
+
+    
