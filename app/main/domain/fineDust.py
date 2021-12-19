efdeabbc1306b40335a4bf25a6524546aaf74a02
gimhanul <gimhanuly@gmail.com>
2021-12-19 22:46:08 +0900

    Solve: 할뻔

--- app/main/domain/fineDust.py
+++ app/main/domain/fineDust.py
@@ -5,12 +5,11 @@ class FineDust(db.Model):
     __tablename__ = 'fine_dust'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}    #한글인식
 
-    datetime = db.Column(db.DateTime, primary_key=True)
+    datetime = db.Column(db.DateTime, default=db.func.current_timestamp(), primary_key=True)
     munge = db.Column(db.Integer)
 
     def __init__(self, munge):
         self.munge = munge
-        self.datetime = datetime.now()
 
     #객체 출력했을 때 나오는 출력화면
     def __repr__(self):
